09733a892542db8dfbdeae5c1cea947b4f77757e
JudeEscarez <123751030+JudeEscarez@users.noreply.github.com>
2023-04-08 15:48:54 +0400

    Added File Method for input and output

--- FilesInOut.java
+++ FilesInOut.java
@@ -13,22 +13,9 @@ import java.lang.Number;
 public class FilesInOut {
 
     public static void main(String[] args) {
-        // Replace this with statements to set the file name (input) and file name (output).
-        // Initially it will be easier to hardcode suitable file names.
-
-        // Set up a new Scanner to read the input file.
-        // Processing line by line would be sensible here.
-        // Initially, echo the text to System.out to check you are reading correctly.
-        // Then add code to modify the text to the output format.
-
-        // Set up a new PrintWriter to write the output file.
-        // Add suitable code into the above processing (because you need to do this line by line also.
-        // That is, read a line, write a line, loop.
-
-        // Finally, add code to read the filenames as arguments from the command line.
-
-        System.out.println("You need to add your own code to do anything");
-
+            //Creates file to be inputed and outputed
+            File inputFile = new File("C:\\Users\\escar\\Downloads\\CSCUT4Practical2-main\\CSCUT4Practical2-main\\inputm.txt");
+            File outputFile = new File("C:\\Users\\escar\\Downloads\\CSCUT4Practical2-main\\CSCUT4Practical2-main\\outputm.txt");
     } // main
 
 } // FilesInOut
